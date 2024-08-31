6de92c8617a1aed04f122e1bcbf2dff80c93225f
kakarlaBalaram <balaramk>
2024-08-31 23:57:39 +0530

    added conversation script

---
+++ 02-converstation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo " ram: : Hi suresh,how r u"
+echo "suresh: :Hello ram. Iam fine"
+echo "ram: : I am doing good.what's going on?"
+echo "suresh: : I startedd learning shell script ram"
