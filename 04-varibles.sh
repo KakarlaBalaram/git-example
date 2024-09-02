b069e543dbca5c629de4ba082c55d17e90a6d339
kakarlaBalaram <balaramk>
2024-09-02 08:06:09 +0530

    i created array

---
+++ 04-varibles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 : : Hi $PERSON2,how r u"
+echo "$PERSON2: :Hello $PERSON1. Iam fine"
+echo "$PERSON1: : I am doing good.what's going on?"
+echo "$PERSON2: : I startedd learning shell script $PERSON1"
---
+++ 05-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#index starts from 0,size is 3
+fruits=("apple" "kiwi" "orange")
+
+echo "first fruits is: ${fruits[0]}"
+echo "second fruits is: ${fruits[1]}"
+echo "third fruits is: ${fruits[2]}"
