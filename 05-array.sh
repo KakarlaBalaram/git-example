b21f932fba48a5ee4258ab15c4ed5fc429316cef
kakarlaBalaram <balaramk>
2024-09-05 21:36:43 +0530

    array out

--- 05-array.sh
+++ 05-array.sh
@@ -6,3 +6,4 @@ fruits=("apple" "kiwi" "orange")
 echo "first fruits is: ${fruits[0]}"
 echo "second fruits is: ${fruits[1]}"
 echo "third fruits is: ${fruits[2]}"
+echo "First fruit is: ${FRUITS[@]}"
