6de19685e9c875f8f829d2eb437eab35266c70f7
kakarlaBalaram <balaramk>
2024-09-06 08:05:17 +0530

    functions

--- 11-installtaion.sh
+++ 11-installtaion.sh
@@ -15,6 +15,13 @@ if [ $USERID -ne 0 ]
     then   
        echo "Git is not installed,going to install it.."
        dnf install git -y
+       if [ $? -ne 0]
+      then 
+         echo "Git installation is not sucess... check it"
+         exit 1
+         else
+           echo "git installation is sucess"
+         fi
      else
        echo "Git is already installed, nothing to do.."
        fi
