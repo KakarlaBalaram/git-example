350db4a3cffc8b0359db559bb17babbdaa6e9932
kakarlaBalaram <balaramk>
2024-09-06 07:14:14 +0530

    functions

---
+++ 10-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [  $NUMBER -gt ] gt,   lt,  eq, -ne
+then
+  echo "Given number: $NUMBER is great than 20"
+else
+  echo "Given number: $NUMBER is lesss than 20"
+  fi
