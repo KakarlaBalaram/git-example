4446c9ef6f77fdba7c10ab239a8597fd48ff263f
kakarlaBalaram <balaramk>
2024-09-06 07:38:28 +0530

    functions

--- 11-installtaion.sh
+++ 11-installtaion.sh
@@ -3,9 +3,14 @@
 USERID=$(id -u)
 #echo "user ID is: $USERID"
 
-if [ $USERID -ne 0 ]
- then
-    echo "please run this script with root priveleges"
-    fi
+# if [ $USERID -ne 0 ]
+#  then
+#     echo "please run this script with root priveleges"
+#     fi
 
-    dnf install git -y
+#     dnf install git -y
+ls -ltr
+
+lsdf -ltr
+
+ls -l
