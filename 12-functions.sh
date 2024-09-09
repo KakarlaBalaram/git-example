2abb82ef0e65dfba66ed7ecda571446d44b7a375
kakarlaBalaram <balaramk>
2024-09-09 17:16:20 +0530

    installtaion

--- 12-functions.sh
+++ 12-functions.sh
@@ -21,35 +21,24 @@ if [ $USERID -ne 0 ]
     dnf  list installed git  
 
     
-    VALIDATE $? "Listing git"
-#     if [ $? -ne 0 ]
-#     then   
-#        echo "Git is not installed,going to install it.."
-#        dnf install git -y
-#        if [ $? -ne 0]
-#       then 
-#          echo "Git installation is not sucess... check it"
-#          exit 1
-#          else
-#            echo "git installation is sucess"
-#          fi
-#      else
-#        echo "Git is already installed, nothing to do.."
-#  fi
+    
+    if [ $? -ne 0 ]
+    then   
+       echo "Git is not installed,going to install it.."
+       dnf install git -y
+       VALIDATE $? "installaing git"
+     else
+       echo "Git is already installed, nothing to do.."
+ fi
 
-#     dnf list installed mysql
+    dnf list installed mysql
 
-#     if [ $? -ne 0 ]
-#     then 
-#        echo "mysql is not installed ...going to install"
-#        dnf install mysql -y
-#        if [ $? -ne 0 ]
-#        then   
-#           echo "mysql installation is falure .. please check"
-#          else
-#            echo " mysql installation is sucess"
-#          fi
-#       else
-#         echo "mysql is already install"
+    if [ $? -ne 0 ]
+    then 
+       echo "mysql is not installed ...going to install"
+       dnf install mysql -y
+       VALIDATE $? "INSTALLING mysql"
+      else
+        echo "mysql is already install"
 
-#       fi
+      fi
